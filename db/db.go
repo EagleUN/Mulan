078d633b59487302be7d65e9d3e6b827984d7441
Sebastian chaves <jschavesr@unal.edu.co>
2019-07-12 05:26:20 +0000

    Set host

--- db/db.go
+++ db/db.go
@@ -13,19 +13,12 @@ import (
 )
 
 func CreateDatabase() (*sql.DB, error) {
-	host, exists := os.LookupEnv("DB_HOST")
-	if !exists {
-		host = "mulan-db"
-	}
-	port, exists := os.LookupEnv("DB_PORT")
-	if !exists {
-		port = "3306"
-	}
+	serverName := "mulan-db:3306"
 	user := "user"
 	password := "password"
 	dbName := "mulan-db"
 
-	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true&multiStatements=true", user, password, host, port, dbName)
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true&multiStatements=true", user, password, serverName, dbName)
 	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
 		return nil, err
